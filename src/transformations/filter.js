3638096051810d31315fcf723c45f83b8a38308d
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2019-08-23 10:44:18 +0200

    fix: insanely slow dropNA and filter

--- src/transformations/filter.js
+++ src/transformations/filter.js
@@ -2,17 +2,30 @@ import getDataLength from '../utils/getDataLength.js'
 
 export default function (data, filterFunction) {
   const length = getDataLength(data)
+  const newColumns = {}
+  for (const colName in data) { newColumns[colName] = [] }
 
-  let i = length
+  // let i = length
 
-  while (i--) {
+  // while (i--) {
+  //   const row = {}
+  //   for (const colName in data) { row[colName] = data[colName][i] }
+
+  //   if (!filterFunction(row, i)) {
+  //     for (const colName in data) {
+  //       data[colName].splice(i, 1)
+  //     }
+  //   }
+  // }
+
+  for (let i = 0; i < length; i++) {
     const row = {}
     for (const colName in data) { row[colName] = data[colName][i] }
 
-    if (!filterFunction(row, i)) {
-      for (const colName in data) {
-        data[colName].splice(i, 1)
-      }
+    if (filterFunction(row, i) === true) {
+      for (const colName in row) { newColumns[colName].push(row[colName]) }
     }
   }
+
+  Object.assign(data, newColumns)
 }
