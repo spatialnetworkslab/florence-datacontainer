a826cb1c7475511dc7e54ab565ea7d0b3de3927e
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2019-09-13 14:50:47 +0200

    feat: addColumn, replaceColumn and deleteColumn

--- src/modifyingRowsAndColumnsMixin.js
+++ src/modifyingRowsAndColumnsMixin.js
@@ -1,12 +1,15 @@
 import produce from 'immer'
 
+import { checkRegularColumnName } from './utils/checkFormat.js'
 import { ensureValidRow, ensureValidRowUpdate, ensureRowExists } from './utils/ensureValidRow.js'
+import { ensureValidColumn, ensureColumnExists } from './utils/isValidColumn.js'
 import { updateDomain } from './utils/calculateDomain.js'
 import { getDataType } from './utils/getDataType.js'
 import getDataLength from './utils/getDataLength.js'
 import { warn } from './utils/logging.js'
 
 const methods = {
+  // Rows
   addRow (row) {
     ensureValidRow(row, this)
     const self = this
@@ -47,10 +50,7 @@ const methods = {
 
     this._data = produce(this._data, draft => {
       for (const columnName in row) {
-        if (columnName === '$key') {
-          warn('Cannot update \'$key\' of row')
-          continue
-        }
+        throwErrorIfColumnIsKey(columnName)
 
         const value = row[columnName]
         draft[columnName][rowNumber] = value
@@ -68,6 +68,8 @@ const methods = {
 
     this._data = produce(this._data, draft => {
       for (const columnName in draft) {
+        throwErrorIfColumnIsKey(columnName)
+
         draft[columnName].splice(rowNumber, 1)
 
         self._resetDomainIfNecessary(columnName)
@@ -75,6 +77,34 @@ const methods = {
     })
   },
 
+  // Columns
+  addColumn (columnName, column) {
+    this._validateNewColumn(columnName, column)
+
+    this._data = produce(this._data, draft => {
+      draft[columnName] = column
+    })
+  },
+
+  replaceColumn (columnName, column) {
+    this.deleteColumn(columnName)
+    this.addColumn(columnName, column)
+  },
+
+  deleteColumn (columnName) {
+    ensureColumnExists(columnName, this)
+    throwErrorIfColumnIsKey(columnName)
+
+    if (Object.keys(this._data).length === 2) {
+      throw new Error('Cannot delete last column')
+    }
+
+    this._data = produce(this._data, draft => {
+      delete draft[columnName]
+    })
+  },
+
+  // Private methods
   _updateDomainIfNecessary (columnName, value) {
     const type = getDataType(value)
 
@@ -91,9 +121,28 @@ const methods = {
     if (columnName in this._domains) {
       delete this._domains[columnName]
     }
+  },
+
+  _validateNewColumn (columnName, column) {
+    checkRegularColumnName(columnName)
+
+    if (columnName in this._data) {
+      throw new Error(`Column '${columnName}' already exists`)
+    }
+
+    const dataLength = getDataLength(this._data)
+    if (dataLength !== column.length) {
+      throw new Error('Column must be of same length as rest of data')
+    }
+
+    ensureValidColumn(column)
   }
 }
 
 export default function (targetClass) {
   Object.assign(targetClass.prototype, methods)
+}
+
+function throwErrorIfColumnIsKey (columnName) {
+  if (columnName === '$key') throw new Error('Cannot modify key column')
 }
