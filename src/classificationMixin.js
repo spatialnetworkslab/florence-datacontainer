244a6854fb545ea0f64edc1f0437fab925a2f719
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2020-11-20 21:18:55 +0100

    feat: fullBounds and boundRanges

--- src/classificationMixin.js
+++ src/classificationMixin.js
@@ -3,6 +3,11 @@ import { scaleThreshold } from 'd3-scale'
 
 const methods = {
   bounds (binInstructions) {
+    const bounds = this.fullBounds(binInstructions)
+    return bounds.slice(1, bounds.length - 1)
+  },
+
+  fullBounds (binInstructions) {
     if (this.type(binInstructions.column) !== 'quantitative') {
       throw new Error('Column should be of type \'quantitative\'')
     }
@@ -12,7 +17,18 @@ const methods = {
       binInstructions
     )
 
-    return bounds.slice(1, bounds.length - 1)
+    return bounds
+  },
+
+  boundRanges (binInstructions) {
+    const bounds = this.fullBounds(binInstructions)
+    const boundRanges = []
+
+    for (let i = 0; i < bounds.length - 1; i++) {
+      boundRanges.push([bounds[i], bounds[i + 1]])
+    }
+
+    return boundRanges
   },
 
   classify (binInstructions, range) {
--- test/classification/classify.test.js
+++ test/classification/classify.test.js
@@ -1,19 +1,6 @@
 import DataContainer from '../../src/index.js'
 
 describe('classify', () => {
-  test('works without providing color scheme', () => {
-    const dataContainer = new DataContainer(
-      { a: [1, 2, 3, 4, 5, 6, 7], b: [8, 9, 10, 11, 12, 13, 14] }
-    )
-
-    const scale = dataContainer.classify({ column: 'a', method: 'EqualInterval', numClasses: 3 })
-
-    expect(scale.range().length).toBe(3)
-    expect(scale(2)).toBe('#deebf7')
-    expect(scale(4)).toBe('#9ecae1')
-    expect(scale(6)).toBe('#3182bd')
-  })
-
   test('works when providing color scheme', () => {
     const dataContainer = new DataContainer(
       { a: [1, 2, 3, 4, 5, 6, 7], b: [8, 9, 10, 11, 12, 13, 14] }
