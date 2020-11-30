3606f9c39073bd523b9a5b6e4eb75a4df9707204
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2020-11-30 20:24:57 +0100

    _

--- src/transformations/pivotLonger.js
+++ src/transformations/pivotLonger.js
@@ -4,8 +4,6 @@ export default function (_data, pivotInstructions) {
   const data = Object.assign({}, _data)
   delete data.$key
 
-  console.log(data)
-
   return into(
     columnOriented.accumulator(),
     pivotLonger(pivotInstructions),
